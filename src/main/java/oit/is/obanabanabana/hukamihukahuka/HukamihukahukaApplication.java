89309e74a1e9cb15bac91bc6eea90a37b37b044f
kuribayashi1026 <e1b18032@st.oit.ac.jp>
2020-10-13 15:06:37 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/obanabanabana/hukamihukahuka/HukamihukahukaApplication.java
@@ -0,0 +1,13 @@
+package oit.is.obanabanabana.hukamihukahuka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HukamihukahukaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HukamihukahukaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/obanabanabana/hukamihukahuka/HukamihukahukaApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.obanabanabana.hukamihukahuka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HukamihukahukaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
