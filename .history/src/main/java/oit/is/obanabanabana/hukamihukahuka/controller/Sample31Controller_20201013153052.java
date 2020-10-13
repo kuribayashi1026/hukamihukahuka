51eadf7a052b50331987ac1c28503e86bfabc760
e1b18049 <e1b18049@oit.ac.jp>
2020-10-13 15:39:33 +0900

    sample31.html GET

---
+++ .history/src/main/java/oit/is/obanabanabana/hukamihukahuka/controller/Sample31Controller_20201013153052.java
@@ -0,0 +1,37 @@
+package oit.is.oabanabana.hukamihukahuka.controller;
+
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+//import oit.is.inudaisuki.springboot_samples.model.Room;
+
+/**
+ * /sample3へのリクエストを扱うクラス authenticateの設定をしていれば， /sample3へのアクセスはすべて認証が必要になる
+ */
+@Controller
+@RequestMapping("/sample3")
+public class Sample31Controller {
+
+  @GetMapping("step1")
+  public String sample31() {
+    return "sample31.html";
+  }
+
+  @GetMapping("step3")
+  public String sample33() {
+    return "sample33.html";
+  }
+
+  @GetMapping("step7")
+  public String sample37() {
+    return "sample37.html";
+  }
+
+}
---
+++ .history/src/main/java/oit/is/obanabanabana/hukamihukahuka/controller/Sample31Controller_20201013153100.java
@@ -0,0 +1,27 @@
+package oit.is.oabanabana.hukamihukahuka.controller;
+
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+//import oit.is.inudaisuki.springboot_samples.model.Room;
+
+/**
+ * /sample3へのリクエストを扱うクラス authenticateの設定をしていれば， /sample3へのアクセスはすべて認証が必要になる
+ */
+@Controller
+@RequestMapping("/sample3")
+public class Sample31Controller {
+
+  @GetMapping("step1")
+  public String sample31() {
+    return "sample31.html";
+  }
+
+}
